a37aaa45c975a89b96bc93e8bbfcae4533f7c9fb
Clemssandra2939 <sclems600@gmail.com>
2024-10-26 11:39:02 +0100

    Function_keywords

---
+++ main.py
@@ -0,0 +1,74 @@
+# functions,code blocks and while loops
+
+# Functions
+# Types of in-built function() of python
+# input(),float(),len(),print(),range(),sum(),str() etc 
+# for example,let us one of the funtions print()
+# print("Hello")
+# num_char =len("Hello")
+# print(num_char)
+
+# What if we want to make our own function...
+# ~we use def keyword(creating and defining our function)
+# ~give our function a name(my_function)
+# eg
+
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+
+# my_function()
+
+# a simple illustration on how function works
+
+# def my_function():
+#     do this
+#     then do this
+#     Finally do this
+
+# To call a function like to excute the code
+
+# def my_function():
+#     do this
+#     then do this
+#     Finally do this
+
+# my_function()
+
+# in the game of the rekgok robot,i got 
+
+# move()
+# move()
+# turn_left()
+# turn_left()
+# move()
+# move()
+# turn_left()
+# turn_left()
+
+# for the the robot to move around in a circle form 
+
+# but wat if they was another function that replace that double turn_left to turn_around since that double turn_left means for the robort to turn around
+# e.g now turn_around is introduced here to replace the double turn_left
+
+# def turn_around():
+#     turn_left()
+#     turn_left()
+
+# move()
+# move()
+# turn_around()
+# move()
+# move()
+# turn_around()
+
+# it will do the same thing this will give : but with turn around  which exist the game before but with the help of def 
+
+# move()
+# move()
+# turn_left()
+# turn_left()
+# move()
+# move()
+# turn_left()
+# turn_left()
